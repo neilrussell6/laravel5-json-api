d1cd1c3b315361500920b143e749a8374a3b9ca5
Neil Russell <neilrussell6@gmail.com>
2017-02-26 20:40:10 +0200

    Update jsonapi config.

--- src/config/jsonapi.php
+++ src/config/jsonapi.php
@@ -2,7 +2,19 @@
 
 return [
     'jwt' => [
-        'error_messages' => [
+        'error_status_code' => [
+            'token_not_provided' => 401,
+            'token_expired' => 401,
+            'token_invalid' => 401,
+            'user_not_found' => 401,
+        ],
+        'error_title' => [
+            'token_not_provided' => "Unauthorised",
+            'token_expired' => "Unauthorised",
+            'token_invalid' => "Unauthorised",
+            'user_not_found' => "Unauthorised",
+        ],
+        'error_detail' => [
             'token_not_provided' => "Access token not provided",
             'token_expired' => "Access token is expired",
             'token_invalid' => "Access token is invalid",
